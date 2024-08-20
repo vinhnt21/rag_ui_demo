6ae8a17b788dc3b8cd38030baae4728e6baccc81
vinhnt21 <vinhnek117@gmail.com>
2024-08-20 14:21:02 +0700

    init

--- src/services/api.chat.js
+++ src/services/api.chat.js
@@ -2,7 +2,7 @@ import {api} from "./api";
 
 export const ask = async (question) => {
     try {
-        return await api.post('chat/ask', {question});
+        return await api.post('ask', {question});
     } catch (error) {
         console.log(error);
     }
