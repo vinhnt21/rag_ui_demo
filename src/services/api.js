e73000dae41f3fc12480ce3c60049214cfaedd0c
vinhnt21 <vinhnek117@gmail.com>
2024-09-17 14:14:29 +0700

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/',
+    baseURL: 'chatbot-api.test.ipcoms.com.vn/',
     headers: {
         'Content-Type': 'application/json',
     },
